feca2dccbe24be750688fd0aec388511d22317e2
Igor.Shavrov <igor.shavrov@qbfin.ru>
2017-08-24 12:31:33 +0300

    done

--- Phase1_openPage.py
+++ Phase1_openPage.py
@@ -18,70 +18,14 @@ soup_task = BeautifulSoup(itog.text,'html.parser')
 
 test = soup_task.find("table",{"class": "table-layout"})
 
-tag_re = re.compile(r'(<!--.*?-->|<[^>]*>)')
-no_tags = tag_re.sub('', user_input)
 
-# file = open('C:/Users/igor.shavrov/Documents/1.txt', 'w+')
-# for row in test.findAll('tr'):
-#     cells = row.findAll("td")
-#     print (cells[:2])
-#     try:
-#         file.write(str(cells[:2])+'\n')
-#     except:
-#         print("TT")
-#         print(cells[:2])
-
-
-
-#pickle.dump(str(cells), file)
-file.close()
-
-# try:
-#     file = pickle.load(open('C:/Users/igor.shavrov/Documents/htmlString.p', 'rb'))
-#     if file == test:
-#         print ('Values not changed!')
-#         sys.exit(0)
-#     else:
-#         pickle.dump (test, open ('C:/Users/igor.shavrov/Documents/htmlString.p', 'wb'))
-#         print ('Saving')
-# except IOError:
-#     pickle.dump(test,open('C:/Users/igor.shavrov/Documents/htmlString.p', 'wb'))
-#     print('Created new file.')
-
-# file = open('C:/Users/igor.shavrov/Documents/i.txt','r')
-# for row in file:
-#     file = row
-# print(file)
-#
-# i = 0
-# for row in test.find_all('tr'):
-#     try:
-#         col = re.findall('<td>(.{10})', str(row))
-#         print(col[0])
-#         if str(file) == str(col[0]):
-#             print("Обновка не требуется")
-#         else:
-#             print("Дата не соответствует. Качаем новый файл")
-#         break
-#     except Exception:
-#         continue
-#
-
-    #col = row.search('<td>(.{10})')
-    #col = re.search('.*', row)
-    #print(col)
-    #print(col.group(1))
-
-#column_1 = col[0].string.strip()
-#date.append(column_1)
-
-# tds = test.find_all('td')
-
-
-# # print(test)
-# datasets = []
-# for row in test.find_all('tr'):
-#   z = row.find_all('td')
-#
-# print(z)
-
+file = open('C:/Users/igor.shavrov/Documents/1.txt', 'w+')
+for row in test.findAll('tr'):
+    cells = row.findAll("td")
+    print (cells[:2])
+    try:
+        file.write(str(cells[:2])+'\n')
+    except:
+        print("TT")
+        print(cells[:2])
+    file.close()
